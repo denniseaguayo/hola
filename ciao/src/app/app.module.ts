2816e2d3cd32840b9d0d27ab9b80509592b0b61a
denniseaguayo <aguayo.dennise@iisgalvanimi.edu.it>
2019-12-03 08:55:15 +0000

    punto2

--- ciao/src/app/app.module.ts
+++ ciao/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { QuartoComponent } from './quarto/quarto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondoComponent,
+    TerzoComponent,
+    QuartoComponent
   ],
   imports: [
     BrowserModule
